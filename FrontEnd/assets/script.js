f00396b915cf9d94810ed812ec6f4b7d51c5615f
3m3line <emeline.boureaud@gmail.com>
2024-05-12 18:34:57 +0200

    MAJ CSS + fin gallery + début filtres

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,6 +1,8 @@
-// code pour gallery
+//variables appelées
 const gallery = document.querySelector('.gallery');
+const portfolio = document.getElementById('portfolio');
 
+// code pour gallery
 async function fetchWork() {
     try{
         const response = await fetch ("http://localhost:5678/api/works");
@@ -15,7 +17,37 @@ async function fetchWork() {
                 <h3>${work.title}</h3>`;
     });
     }catch (error){console.error ('une erreur s\'est produite pendant la récupération des données')}
-}
-fetchWork()
+};
+fetchWork();
 
 // filtres
+async function filter(){
+    try{
+        const response = await fetch ("http://localhost:5678/api/categories")
+        console.log(response.status)
+        const data = await response.json()
+        console.log("affichage data",data)
+        const filterWorkParent = document.createElement ('div');
+        portfolio.appendChild(filterWorkParent);
+        filterWorkParent.classList.add ('filter')
+        portfolio.parentNode.insertBefore(filterWorkParent, gallery);
+        Object.values(data).forEach(category => {
+            const button = document.createElement('button');
+            button.textContent = category.name;
+            button.classList.add('filter-button');
+            filterWorkParent.appendChild(button);
+            button.addEventListener("click", function() {
+                const filterValue = this.dataset.filter;
+                gallery.querySelectorAll('figure').forEach(figure => {
+                    if (filterValue === 'all' || figure.classList.contains(filterValue)) {
+                        figure.style.display = 'block';
+                    } else {
+                        figure.style.display = 'none';
+                    }
+                });
+            });
+        })
+    }
+    catch (error){console.error ('une erreur s\'est produite pendant la récupération des données')}
+}
+filter()
